def46cb5f3afc2ae059f5b4fa00a1dbe206c681f
Koustav Manna <koustaviitjee@gmail.com>
2026-02-21 11:41:42 +0530

    Added user Table to database using django orm

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Resume(models.Model):
+    
+    name = models.CharField(max_length=255)
+
+    email = models.EmailField()
+
+    file = models.FileField(upload_to="resumes/")
+
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
